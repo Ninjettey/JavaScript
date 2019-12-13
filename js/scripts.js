5c48e8e85f874beac1f7f5af2b3700564861658b
Jennifer White <ninjettey@gmail.com>
2019-12-12 20:52:59 -0800

    add javascript

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+// Numbers
+// numbers and be computed with +, -, *, /, %, ++ increment, -- decrement
+// NaN means Not a Number
+var pizza = 5;
+var soda = 1;
+var bread = 3;
+
+var total = pizza + soda;
+
+console.log(total);
+
+/**
+ * The console .log() is a function in JavaScript which is used to print any kind of variables defined before in it or to just print any message that needs to be displayed to the user.
+ */
+
+ // String
+
+ const name = 'Jennifer White';
+
+ const welcome = 'Welcome to Geekwise Academy';
+
+ console.log(name);
+ console.log(welcome);
+
+ // Array
+ var fruit = ['apples', 'grapes', 'oranges', 'bananas'];
+ var rockBand = ['Motionless in White', 'In This Moment', 'New Years Day', 'Hollywood Vampires'];
+
+ console.log(fruit);
+ console.log(rockBand);
+
+ // Object
+
+ var car = {
+     type: "Fiat",
+     model: "500",
+     color: "white"
+ };
+
+console.log(car);
+
+// Boolean
+// everything with a real value is true 0, null, undefined is false
