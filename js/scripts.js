ecc1b9fc4854acd7f7dc8b078c5148f186c5947d
Jennifer White <ninjettey@gmail.com>
2019-12-17 20:55:31 -0800

    back to top button

--- js/scripts.js
+++ js/scripts.js
@@ -41,3 +41,37 @@ console.log(car);
 
 // Boolean
 // everything with a real value is true 0, null, undefined is false
+// comparison operators always return true or false ==, ===, !=, !==, !===, <, >, <=, >=, && AND, || OR, ! NOT
+const present = true;
+const asbent = false;
+Boolean(present);
+console.log(present);
+// Undefined
+var cat = ['Tabby', 'Calico', 'Tiger', 'Lion']
+console.log(cat);
+// Null
+var person = {firstName: "Jennifer", lastName: "White", age:38, eyeColor:"Brown"};
+person = null;    // Now value is null, but type is still an object
+console.log(person);
+//Get the button
+var mybutton = document.getElementById("myBtn");
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+document.write('Hello World');
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+// command question mark is a single line comment
+
+var favoriteMovie ='My favorite movies Harry Potter series'
+document.write(favoriteMovie);
